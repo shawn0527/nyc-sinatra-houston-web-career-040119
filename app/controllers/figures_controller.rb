166ae88f145ae9f6c6ddcca7d45230859e751e55
Shawn Ren <shawnren527@gmail.com>
2019-04-25 22:37:27 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,28 +13,29 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+  #   figure = Figure.new(params[:figure])
+  #   unless params[:title][:name].empty?
+  #     figure.titles << Title.create(params(:title))
+  #   end
+  #
+  #   unless params[:landmark][:name].empty?
+  #     figure.landmarks << Landmark.new(params[:landmark])
+  #   end
+  #   figure.save
+  # end
     figure = Figure.new(params[:figure])
     unless params[:title][:name].empty?
-      figure.titles << Title.create(params(:title))
+      title = Title.create(params[:title])
+      figure.titles << title
+
     end
 
     unless params[:landmark][:name].empty?
-      figure.landmarks << Landmark.new(params[:landmark])
+      landmark = Landmark.create(params[:landmark])
+      figure.landmarks << landmark
     end
     figure.save
   end
-    # figure = Figure.new(params[:figure])
-    # unless params[:title][:name].empty?
-    #   title = Title.create(params[:title])
-    #   figure.titles << title
-    #
-    # end
-    #
-    # unless params[:landmark][:name].empty?
-    #   landmark = Landmark.create(params[:landmark])
-    #   figure.landmarks << landmark
-    # end
-    # figure.save
 
 
 end
