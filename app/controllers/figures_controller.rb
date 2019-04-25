d8ad4bc740d5caab7d5210cd034729fb65099507
Shawn Ren <shawnren527@gmail.com>
2019-04-25 22:43:49 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,7 +13,7 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    figure = Figure.new(params[:figure])
+    figure = Figure.create(params[:figure])
     unless params[:title][:name].empty?
       figure.titles << Title.create(params(:title))
     end
