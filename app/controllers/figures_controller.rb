cc67c35063ef008631da01d997d55b2a1db50cb8
Shawn Ren <shawnren527@gmail.com>
2019-04-25 21:28:52 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,16 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get'/figures' do
+    @figure = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+
 end
